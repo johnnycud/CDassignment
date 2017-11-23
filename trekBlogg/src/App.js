5823ce77611501801a7e062d07df8d6f37fd5598
jcuddles@msn.com <jcuddles@msn.com>
2017-11-23 18:28:22 +0000

    HackerNews - Static version completed

--- trekBlogg/src/App.js
+++ trekBlogg/src/App.js
@@ -1,21 +1,85 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
 
-class App extends Component {
+class Entry extends React.Component {
   render() {
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+      <form style={{ marginTop: '30px' }}>
+        <h3>Add a new blogg</h3>
+
+        <div className="form-group">
+          <input type="text"
+            className="form-control"
+            placeholder="Title"></input>
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <div className="form-group">
+          <input type="text"
+            className="form-control"
+            placeholder="Link"></input>
+        </div>
+        <button type="submit" className="btn btn-primary">Blogg</button>
+      </form>
+    );
+  }
+}
+
+class BloggItem extends React.Component {
+  render() {
+    var divStyle = {
+      fontSize: '20px',
+      marginLeft: '10px'
+    };
+    var cursor = { cursor: 'pointer' };
+    var line;
+    if (this.props.blogg.link) {
+      line = <a href={this.props.blogg.link} >
+        {this.props.blogg.title} </a>;
+    } else {
+      line = <span>{this.props.blogg.title} </span>;
+    }
+    return (
+      <div >
+        <span className="glyphicon glyphicon-thumbs-up"
+          style={cursor} />
+        {this.props.blogg.upvotes}
+        <span style={divStyle} >{line}<span>
+          <a href={'#/bloggs/' + this.props.blogg.id}>Comments</a>
+        </span>
+        </span>
       </div>
     );
   }
 }
 
-export default App;
+class BloggList extends React.Component {
+  render() {
+    var content = this.props.bloggs.map(function (blogg, index) {
+      return <BloggItem key={index} blogg={blogg} />;
+    });
+    return (
+      <div>
+        {content}
+      </div>
+    );
+  }
+}
+
+class BloggApp extends React.Component {
+  render() {
+    return (
+      <div className="container">
+        <div className="row">
+          <div className="col-md-6 col-md-offset-3">
+            <div className="page-header">
+              <h1>Blogg News</h1>
+              <BloggList bloggs={this.props.bloggs}/>  
+              <Entry />
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default BloggApp;
+
